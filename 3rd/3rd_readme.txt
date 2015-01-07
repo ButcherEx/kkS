0bb2f9a073ed55f5445ed7e60bd941ff9b9258c2
snail <mawh583@163.com>
2015-01-07 16:09:27 +0800

    3rd

---
+++ 3rd/3rd_readme.txt
@@ -0,0 +1,6 @@
+gperftools-2.2.1
+jemalloc (./jemallc->3.3.1)(jem -3.6.0)
+lua-5.2.3
+boost_1_55_0
+
+proto buffers https://github.com/google/protobuf/releases
---
+++ 3rd/jem/include/jemalloc/jemalloc_defs.h
@@ -0,0 +1,29 @@
+/* include/jemalloc/jemalloc_defs.h.  Generated from jemalloc_defs.h.in by configure.  */
+/* Defined if __attribute__((...)) syntax is supported. */
+/* #undef JEMALLOC_HAVE_ATTR */
+
+/* Support the experimental API. */
+#define JEMALLOC_EXPERIMENTAL 
+
+/*
+ * Define overrides for non-standard allocator-related functions if they are
+ * present on the system.
+ */
+/* #undef JEMALLOC_OVERRIDE_MEMALIGN */
+/* #undef JEMALLOC_OVERRIDE_VALLOC */
+
+/*
+ * At least Linux omits the "const" in:
+ *
+ *   size_t malloc_usable_size(const void *ptr);
+ *
+ * Match the operating system's prototype.
+ */
+#define JEMALLOC_USABLE_SIZE_CONST const
+
+/* sizeof(void *) == 2^LG_SIZEOF_PTR. */
+#if defined(X64_PLATFORM)
+#	define LG_SIZEOF_PTR 3
+#else
+#	define LG_SIZEOF_PTR 2
+#endif
