90ae20b3b69f50cd9c5a7e8e2d02a8bedada2dd3
snail <mawh583@163.com>
2016-01-21 17:31:19 +0800

    make_erlc.bat

--- ErlS/make_erlc.bat
+++ ErlS/make_erlc.bat
@@ -1,7 +1,7 @@
 
 echo off
 echo making
-for /r src %%f in (*.erl) do erlc +debug_info -I include -o ebin %%f
+for /r ./ %%f in (*.erl) do erlc +debug_info -I include -o ebin %%f
 del /S /Q .\app\*.beam
 copy ebin\*.* app\ebin\
 pause
