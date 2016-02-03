00517dea2e5d73aa1777492b765cd841a042931e
snail <mawh583@163.com>
2016-02-03 11:51:35 +0800

    -- delete

--- ErlS/make_erlc.bat
+++
@@ -1,7 +0,0 @@
-
-echo off
-echo making
-for /r ./ %%f in (*.erl) do erlc +debug_info -I include -o ebin %%f
-del /S /Q .\app\*.beam
-copy ebin\*.* app\ebin\
-pause
--- ErlS/make_rebar.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-del /S /Q .\app\*.beam
-rebar compile
-copy ebin\*.* app\ebin\
-pause
